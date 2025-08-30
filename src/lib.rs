792f594147b5105db727e23d317797af0ae26f83
Your Name <you@example.com>
2025-08-30 08:13:30 +0200

    disable LP for now due to bugs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
-
 pub mod astar;
-pub mod linear_programming;
-pub mod linear_programming_helpers;
-pub mod linear_programming_lu;
-pub mod linear_programming_ordering;
-pub mod linear_programming_solver;
-pub mod linear_programming_sparse;
+// pub mod linear_programming;
+// pub mod linear_programming_helpers;
+// pub mod linear_programming_lu;
+// pub mod linear_programming_ordering;
+// pub mod linear_programming_solver;
+// pub mod linear_programming_sparse;
+pub mod abnormal_fraction;
 pub mod network_simplex;
 pub mod network_simplex_value_type;
-pub mod abnormal_fraction;
